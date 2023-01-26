3dbd4a967083afe562408af5194b067a9e3bb481
DongiCoding <loveehd012@gmail.com>
2023-01-26 20:03:15 +0900

    특정 게시글 삭제 완료, 수정 기능 구현중 화면단, jquery문은 완성 controller 해야함 2023/01/26 20:03

--- boards/src/main/java/com/dongi/boards/controller/BrdController.java
+++ boards/src/main/java/com/dongi/boards/controller/BrdController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -80,11 +80,12 @@ public class BrdController {
 	}
 	
 	// 특정 게시글 삭제
-	@DeleteMapping("/dltBrd/{brdNm}")
-	public void dltBrd(@PathVariable int brdNm, HttpServletResponse response) throws IOException {
+	@DeleteMapping("/dltBrd")
+	public void dltBrd(@RequestParam("brdNm") int brdNm ) {
 		brdService.dltBrd(brdNm);
-		
-		response.sendRedirect("/brd/brdList");
 	}
+	
+	// 특정 게시글 수정
+
 	
 }
